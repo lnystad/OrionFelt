698299c941c26fa3308d1389b5335900bf41d87e
Lars-Andreas Nystad <lan@dips.no>
2016-01-15 08:29:51 +0100

    fix structure

--- OrionLag/Server/OrionLag.Service.Server/Properties/AssemblyInfo.cs
+++ OrionLag/Server/OrionLag.Service.Server/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrionLag.Service.Server")]
+[assembly: AssemblyTitle("OrionLag.Service.Servers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DIPS ASA")]
-[assembly: AssemblyProduct("OrionLag.Service.Server")]
+[assembly: AssemblyProduct("OrionLag.Service.Servers")]
 [assembly: AssemblyCopyright("Copyright © DIPS ASA 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,25 +19,8 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page, 
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page, 
-                                              // app, or any theme specific resource dictionaries)
-)]
-
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bbd36083-ea3a-4e89-8eb1-3ed9d89bdb0f")]
 
 // Version information for an assembly consists of the following four values:
 //
